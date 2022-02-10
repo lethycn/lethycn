f7a1519968e73d3c8eb4037d101cdce351e64913
lethycn <98864100+lethycn@users.noreply.github.com>
2022-02-09 22:31:22 -0600

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,41 +1,66 @@
+var path,boy, leftBoundary,rightBoundary;
+var pathImg,boyImg;
+var i;
 
-function setup() {
-  createCanvas(400,400);
-  background(51);
-  box = createSprite(200,200,30,30);
-
+function preload(){
+  pathImg = loadImage("path.png");
+  boyImg = loadAnimation("Runner-1.png","Runner-2.png");
 }
 
-function draw() 
-{
-
-  // escribir el código para cambiar el color del fondo 
-  // rojo cuando se presiona RIGHT_ARROW (tecla de flecha derecha)
+function setup(){
   
+  createCanvas(400,400);
+  
+// Mover el fondo
+path=createSprite(200,200);
+path.addImage(pathImg);
+path.velocityY = 4;
+path.scale=1.2;
+
+//crear sprite de niño corriendo
+boy = createSprite(180,340,30,30);
+boy.scale=0.08;
+boy.addAnimation("JakeRunning",boyImg);
   
 
-  if (keyIsDown(LEFT_ARROW)) 
-  {
-    background("blue");
-    
-  }
- 
-    if (keyIsDown(UP_ARROW)) 
-  {
-    background("yellow");
+leftBoundary=createSprite(0,0,100,800);
+
+// leftBoundary.invisible = false;
+// leftBoundary.visible = true;
+// leftBoundary.invisible = true;
+// leftBoundary.visible = false;
+
+
+rightBoundary=createSprite(410,0,100,800);
+rightBoundary.visible = false;
+}
+
+function draw() {
+  background(0);
+  path.velocityY = 4;
+  
+  boy.x = World.mouseX;
+  
+  edges= createEdgeSprites();
+  boy.collide(edges[3]);
+  boy.collide(leftBoundary);
+  boy.collide(rightBoundary);
+  
+  //código para reiniciar el fondo
+
+  /*if(path.y > 400 ){
+    path.y = height/2;
+  }*/
+
+  /*if(path.y > 400 ){
    
-  }
+  path.y = height/2;
+  }*/
 
-  if (keyIsDown(DOWN_ARROW)) 
-  {
-    background("green");
-  }
+  /*if(path.y > 400 ){
+path.y = height/2;}*/
 
-  if (keyIsDown(RIGHT_ARROW)) 
-  {
-    background("red");
-  }
+/*if(path.y > 400 ){path.y = height/2;}*/
   
   drawSprites();
 }
-
