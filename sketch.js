69b5861dffed64d04f77183c87be345c9ed849de
lethycn <98864100+lethycn@users.noreply.github.com>
2022-02-15 22:21:54 -0600

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,66 +1,47 @@
-var path,boy, leftBoundary,rightBoundary;
-var pathImg,boyImg;
-var i;
+var sea,ship;
+var seaImg,shipImg;
 
 function preload(){
-  pathImg = loadImage("path.png");
-  boyImg = loadAnimation("Runner-1.png","Runner-2.png");
+  //descomentar el código para agregar la animación del barco 
+
+  shipImg1 = loadAnimation("ship-1.png");
+  //shipImg1 = loadAnimation("ship-1.png");
+  //shipImg1 = loadAnimation("ship-1");
+  //shipImg1 = loadAnimation("ship-1.png","ship-2.png","ship-1.png","ship-2.png");
+  //shipImg1 = loadAnimation("ship-1","ship-2","ship-1","ship-2");
+  
+  seaImg = loadImage("sea.png");
 }
 
 function setup(){
-  
   createCanvas(400,400);
+  background("blue");
+
+  // Mover el fondo
+  sea=createSprite(400,200);
+  sea.addImage(seaImg);
+  sea.velocityX = -5;
+  sea.scale=0.3;
+
   
-// Mover el fondo
-path=createSprite(200,200);
-path.addImage(pathImg);
-path.velocityY = 4;
-path.scale=1.2;
-
-//crear sprite de niño corriendo
-boy = createSprite(180,340,30,30);
-boy.scale=0.08;
-boy.addAnimation("JakeRunning",boyImg);
+  ship = createSprite(130,200,30,30);
+  ship.addAnimation("movingShip",shipImg1);
+  ship.scale =0.25;
   
-
-leftBoundary=createSprite(0,0,100,800);
-
-// leftBoundary.invisible = false;
-// leftBoundary.visible = true;
-// leftBoundary.invisible = true;
-// leftBoundary.visible = false;
-
-
-rightBoundary=createSprite(410,0,100,800);
-rightBoundary.visible = false;
 }
 
 function draw() {
   background(0);
-  path.velocityY = 4;
-  
-  boy.x = World.mouseX;
-  
-  edges= createEdgeSprites();
-  boy.collide(edges[3]);
-  boy.collide(leftBoundary);
-  boy.collide(rightBoundary);
-  
-  //código para reiniciar el fondo
+  sea.velocityX = -3;
 
-  /*if(path.y > 400 ){
-    path.y = height/2;
-  }*/
+  //descomentar el código para reiniciar el fondo
+  if(sea.x < 0){
+    //sea.x = 0;
+    //sea.x = sea.width;
+    //sea.x = sea.width/8;
+    //sea.y = height;
+  }
 
-  /*if(path.y > 400 ){
-   
-  path.y = height/2;
-  }*/
-
-  /*if(path.y > 400 ){
-path.y = height/2;}*/
-
-/*if(path.y > 400 ){path.y = height/2;}*/
-  
+ 
   drawSprites();
 }
